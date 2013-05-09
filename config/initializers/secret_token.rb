76bbd47c0f10356485f5c7ef9b712008e40d1062
Billy Shih <me@billyshih.com>
2013-05-08 17:35:03 -0700

    Setup rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+LunchTracker::Application.config.secret_token = '22e7670d3d823b509c0567677e178bb67fb1cf1fdbab907a56139e96894ed90af1ad964e73c16afa583b88c8e5eb54635ade2a3554ce1e33c3d794808a7d68b1'
